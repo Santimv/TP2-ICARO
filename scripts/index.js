d9368a8ff70b87da301cf8f8ba782b046262e4da
Santimv <santimv01@gmail.com>
2021-10-04 02:41:56 -0300

    users, user, home and contact pages finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,12 @@
-// Variables de HTML
 const users = document.querySelector("#users");
+const btnEnviarContacto = document.querySelector("#btn-send");
 
 let userInfo =
-  '<h1 id="username"></h1><div class="card-info"><label></label><label></label><label></label><label></label></div><div><button class= "btnViewUser">Visitar perfil</button></div></div><footer><div class="posts"><p><strong>Posteos:</strong></p><p>10</p></div></footer>';
+  '<h1 id="username"></h1><div class="card-info"><label></label><label></label><label></label><label></label></div><div><button class= "btnViewUser">Visitar perfil</button></div></div><footer><div class="posts-counter"><p><strong>Posteos:</strong></p><p>10</p></div></footer>';
 let arrUser = [];
 let loading = true;
 let userList = document.getElementById("users");
 
-// Crear elementos de user
 const createCardUserElement = (id, user) => {
   const element1 = document.createElement("div");
   element1.classList.add("card");
@@ -27,7 +26,6 @@ const createCardUserElement = (id, user) => {
   return element1;
 };
 
-// Llamado a la API
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((res) => res.json())
   .then((data) => {
@@ -41,10 +39,6 @@ fetch("https://jsonplaceholder.typicode.com/users")
     }
   })
   .catch((err) => console.log("ERROR: ", err));
-
-// fetch("https://jsonplaceholder.typicode.com/posts?userId=1")
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
 
 async function selectUser(e) {
   if (e.target.classList.contains("btnViewUser")) {
@@ -69,4 +63,18 @@ function getUserData(id) {
   } catch (error) {
     console.error(error);
   }
+}
+
+// CONTACTO
+
+btnEnviarContacto?.addEventListener("click", sendContact);
+
+function sendContact(e) {
+  e.preventDefault();
+  let nameContact = document.querySelector("#input-name").value;
+  let emailContact = document.querySelector("#input-email").value;
+  let phoneContact = document.querySelector("#input-phone").value;
+  let mgContact = document.querySelector("#input-msg").value;
+  let contactJson = { nameContact, emailContact, phoneContact, mgContact };
+  console.log(contactJson);
 }
--- scripts/users.js
+++ scripts/users.js
@@ -1,17 +1,23 @@
 const userProfile = document.querySelector("#profileUser");
-
-const userData =
-  '<h2 class="tittle1">Santiago</h2><div class="profile-info"><div class="data"><h3 class="tittle2">Datos Personales</h3><label></label><label></label><label></label><label></label></div><div class="data"><h3 class="tittle2">Ubicación</h3><label></label><label></label><label></label><label></label></div><div class="data"><h3 class="tittle2">Compañía</h3><label></label><label></label></div></div>';
+const posts = document.querySelector("#posts");
+let arrPosts = [];
+const userTemplate =
+  '<h2 class="tittle1">Santiago</h2><div class="profile-info"><div class="data"><h3 class="tittle2">Datos Personales📝</h3><label></label><label></label><label></label><label></label></div><div class="data"><h3 class="tittle2">Ubicación📌</h3><label></label><label></label><label></label><label></label></div><div class="data"><h3 class="tittle2">Compañía🏢</h3><label></label><label></label></div></div>';
+const postTemplate = '<div class="post"><h4></h4><span></span></div>';
+let userData;
 
 if (localStorage.getItem("userSelected")) {
-  let userProfile = JSON.parse(localStorage.getItem("userSelected"));
-  createProfileUserElement(userProfile);
+  userData = JSON.parse(localStorage.getItem("userSelected"));
+  createProfileUserElement(userData);
+  getPosts();
+} else {
+  alert("Ha ocurrido un error al intentar cargar el perfil");
 }
 
 function createProfileUserElement(user) {
   userProfile.appendChild(document.createElement("div"));
 
-  userProfile.innerHTML = userData;
+  userProfile.innerHTML = userTemplate;
 
   userProfile.children[0].innerText = user.username;
   userProfile.children[1].children[0].children[1].innerText =
@@ -34,6 +40,32 @@ function createProfileUserElement(user) {
     "Nombre Social: " + user.company.name;
   userProfile.children[1].children[2].children[2].innerText =
     "Eslogan: " + user.company.catchPhrase;
-
   return userProfile;
+}
+
+const createPostElement = (post, i) => {
+  const postElement = document.createElement("div");
+  postElement.innerHTML = postTemplate;
+
+  postElement.children[0].children[0].innerText = i + ". " + post.title;
+  postElement.children[0].children[1].innerText = post.body;
+
+  postElement.id = "post-" + post.id;
+
+  return postElement;
+};
+
+function getPosts() {
+  try {
+    fetch("https://jsonplaceholder.typicode.com/posts?userId=" + userData.id)
+      .then((response) => response.json())
+      .then((jsonData) => {
+        arrPosts = jsonData;
+        arrPosts.forEach((x, i) => {
+          posts.appendChild(createPostElement(x, i + 1));
+        });
+      });
+  } catch (err) {
+    return console.log("ERROR: ", err);
+  }
 }
